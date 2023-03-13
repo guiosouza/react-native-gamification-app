a29f351bf11e64ae7d8c91a7a8f9e32ca192a017
Guilherme <78989152+guiosouza@users.noreply.github.com>
2023-03-13 11:02:03 -0300

    Added: ScrollView, FlatList. Splited components

--- Projects/first_project/App.js
+++ Projects/first_project/App.js
@@ -1,19 +1,32 @@
 import React from "react";
 import { useState } from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  Button,
+  StyleSheet,
+  TextInput,
+  View,
+  FlatList,
+} from "react-native";
+
+import GoalItem from "./components/GoalItem";
 
 const App = () => {
+  // setting entered goal
   const [enteredGoalText, setEnteredGoalText] = useState("");
+
+  // list of goals - change dynamically
   const [courseGoals, setCourseGoals] = useState([]);
 
+  // whenever text chance - new text is set to the const
   const goalInputHandler = (exteredText) => {
     setEnteredGoalText(exteredText);
   };
 
+  // take a list of goals and add a new one
   const addGoalHandler = () => {
     setCourseGoals((currentCourseGoals) => [
       ...currentCourseGoals,
-      enteredGoalText,
+      {text: enteredGoalText, id: Math.random().toString()},
     ]);
   };
 
@@ -28,11 +41,16 @@ const App = () => {
         <Button title="Add goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        {courseGoals.map((goal) => (
-          <View key={goal} style={styles.goalItem}>
-            <Text style={styles.goalText}>{goal}</Text>
-          </View>
-        ))}
+        <FlatList
+          data={courseGoals}
+          renderItem={(itemData) => {
+            return <GoalItem />
+          }}
+          keyExtractor={(item, index) =>  {
+            return item.id;
+          }}
+          alwaysBounceVertical={false}
+        />
       </View>
     </View>
   );
@@ -62,16 +80,6 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 5,
-  },
-  goalItem: {
-    margin: 8,
-    padding: 8,
-    borderRadius: 6,
-    backgroundColor: "#5e0acc",
-    
-  },
-  goalText: {
-    color: "white",
   }
 });
 
---
+++ Projects/first_project/components/GoalInput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { VStack } from 'native-base';
+
+const Goalinput = () => {
+  return (
+    <VStack>
+
+    </VStack>
+  );
+};
+
+export default Goalinput;
