2230868ece4a7574df5d9f30becc097700c79731
unknown <guilhermedeoliveiradesouza@gmail.com>
2025-02-26 10:43:22 -0300

    repo re-start, first commit

--- Projects/login-screen/App.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import LoginScreen from "./screens/Login";
-
-export default function App() {
-  return <LoginScreen />;
-}
--- Projects/login-screen/README.md
+++
@@ -1,1 +0,0 @@
-![image](https://user-images.githubusercontent.com/78989152/225321052-c9cfc6a5-3fcb-4240-8b00-7a6959911e7c.png)
